c998611adb3e3f5505e013b2f4467f4f2fc185e4
“H0mePage” <gauravsuryavanshi7484@gmail.com>
2022-07-10 11:58:09 +0530

    first commit

--- index.js
+++ index.js
@@ -1,38 +1,49 @@
-// const closedFace= document.querySelector('.closed')
-// //for id s use # class . to select by const 
-// const openFace=document.querySelector('.open')
+const closedFace= document.querySelector('.closed');
+//for id s use # class . to select by const 
+const openFace=document.querySelector('.open');
 
-// //AddEvent Listner
+//AddEvent Listner
 
-// closedFace.addEventListener('click',()=> {
-//     if(openFace.classList.contains('open')){
-//         openFace.classList.add('active')
-//         closedFace.classList.remove('active')
-//     }
-  
+closedFace.addEventListener('click',()=> {
+    if(openFace.classList.contains('open')){
+        openFace.classList.add('active');
+        closedFace.classList.remove('active');
+    }
+});
+openFace.addEventListener('click',()=> {
+    if(closedFace.classList.contains('closed')){
+        closedFace.classList.add('active');
+        openFace.classList.remove('active');
+    }
+});
 
-// })
-// openFace.addEventListener('click',()=> {
-//     if(closedFace.classList.contains('open')){
-//         closedFace.classList.add('active')
-//         openFace.classList.remove('active')
-//     }
-// })
-const closedFace = document.querySelector('.closed');
-const openFace = document.querySelector('.open');
+const circle = document.querySelector('#circle');
 
-// Add event listener
-closedFace.addEventListener('click', () => {
-  if (openFace.classList.contains('open')) {
-    openFace.classList.add('active');
-    closedFace.classList.remove('active');
+circle.addEventListener('mouseenter', () => {
+  if (!circle.classList.contains('hover')) {
+    circle.classList.add('hover');
   }
 });
 
-openFace.addEventListener('click', () => {
-  if (closedFace.classList.contains('closed')) {
-    closedFace.classList.add('active');
-    openFace.classList.remove('active');
+circle.addEventListener('mouseleave', () => {
+  if (circle.classList.contains('hover')) {
+    circle.classList.remove('hover');
   }
 });
+// const closedFace = document.querySelector('.closed');
+// const openFace = document.querySelector('.open');
 
+// // Add event listener
+// closedFace.addEventListener('click', () => {
+//   if (openFace.classList.contains('open')) {
+//     openFace.classList.add('active');
+//     closedFace.classList.remove('active');
+//   }
+// });
+
+// openFace.addEventListener('click', () => {
+//   if (closedFace.classList.contains('closed')) {
+//     closedFace.classList.add('active');
+//     openFace.classList.remove('active');
+//   }
+// });
